d130b1f3ccb6ae645d7f0f005d6bf4f7764a723a
Akarkab-Maryam <ourzazat.ne1998@gmail.com>
2025-01-12 13:00:01 +0100

    Add files via upload

---
+++ lancerScript.bat
@@ -0,0 +1,6 @@
+@echo off
+rem Le chemin du dossier où se trouve votre script
+cd /d "C:\Users\Destock-afric\Desktop\Soutennance"
+rem Exécution du script Node.js
+node "C:\Users\Destock-afric\Desktop\Soutennance\Recole.js"
+pause
